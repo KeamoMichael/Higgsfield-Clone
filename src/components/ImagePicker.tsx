31eaba90bf90d4d802cab00fbf9464361822386e
keamoMichael <michaelkeamo@gmail.com>
2026-01-19 10:28:24 +0200

    Swap label and description rendering order with description on top

--- src/components/ImagePicker.tsx
+++ src/components/ImagePicker.tsx
@@ -82,10 +82,10 @@ function ImagePicker({
                         >
                             <img src={option.image} alt={option.label} loading="lazy" />
                             <div className="picker-item-overlay">
-                                <span className="picker-item-label">{option.label}</span>
                                 {option.description && (
                                     <span className="picker-item-description">{option.description}</span>
                                 )}
+                                <span className="picker-item-label">{option.label}</span>
                             </div>
                             {selectedValue === option.value && (
                                 <div className="picker-item-check">
